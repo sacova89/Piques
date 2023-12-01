32699f412c0566a733d119099c04b6ffe45fe156
Santiago Acosta <sacova89@gmail.com>
2023-12-01 08:12:45 -0500

    Fetch: Se agregaron cambios en los Scrpits de UIManager y RaceManager para la funcionalidad de posición de del Jugador Principal, adicional se agradaron los CheckPoints y modificaciones en el terreno 1.12.2023 8:12

--- Assets/scripts/RaceManager.cs
+++ Assets/scripts/RaceManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,8 +8,14 @@ public class RaceManager : MonoBehaviour
     public static RaceManager instance;
 
     public Checkpoint[] allCheckpoints;
+
     public int totalLaps;
-    
+
+    public CarController playerCar;
+    public List<CarController> allAICars = new List<CarController>();
+    public int playerPosition;
+    public float timeBetweenPosCheck = .2f;
+    private float posChkCounter;
 
     private void Awake()
     {
@@ -31,8 +37,36 @@ public class RaceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-   
+        posChkCounter -= Time.deltaTime;
+        if (posChkCounter <= 0)
+        {
+
+            playerPosition = 1;
+
+        foreach (CarController aiCar in allAICars)
+        {
+            if (aiCar.currentLap > playerCar.currentLap)
+            {
+                playerPosition++;
+            }
+            else if (aiCar.currentLap == playerCar.currentLap)
+            {
+                if (aiCar.nextCheckpoint > playerCar.nextCheckpoint)
+                {
+                    playerPosition++;
+                }
+                else if (aiCar.nextCheckpoint == playerCar.nextCheckpoint)
+                {
+                    if (Vector3.Distance(aiCar.transform.position, allCheckpoints[aiCar.nextCheckpoint].transform.position) < Vector3.Distance(playerCar.transform.position, allCheckpoints[aiCar.nextCheckpoint].transform.position))
+                    {
+                        playerPosition++;
+                    }
+                }
+            }
+        }
+            posChkCounter = timeBetweenPosCheck;
+            UIManager.instance.positionText.text = playerPosition + "/" + (allAICars.Count + 1);
+        }
+    }
 }
